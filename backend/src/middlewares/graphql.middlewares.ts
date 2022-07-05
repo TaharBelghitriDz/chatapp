a9db680985c27e5e4e91a4f1c258510f523371e2
tahar belghitri <103012653+TaharBelghitriDz@users.noreply.github.com>
2022-07-06 00:59:18 +0100

    Update graphql.middlewares.ts

--- backend/src/middlewares/graphql.middlewares.ts
+++ backend/src/middlewares/graphql.middlewares.ts
@@ -6,20 +6,27 @@ export const checkUser: IMiddlewareFunction = async (
   resolve,
   parent,
   args,
-  context,
+  cntx,
   info
 ) => {
   try {
-    const checkToken: any = tokenVrfy(args.token);
+    const { headers } = cntx;
+    console.log(headers);
+
+    if (!headers.token) return { err: "unvalid user #1" };
+
+    const checkToken: any = tokenVrfy(headers.token);
     delete args["token"];
+
     return user.findUser({ _id: checkToken.str }, async (user) => {
       if (user) {
         args.userData = user;
-        return await resolve(parent, args, context, info);
-      } else return { err: "unvalid user" };
+        return await resolve(parent, args, cntx, info);
+      } else return { err: "unvalid user #2" };
     });
-  } catch {
+  } catch (err) {
     //loging here
-    return { err: "unvalid user" };
+    console.log(err);
+    return { err: "unvalid user #3" };
   }
 };
