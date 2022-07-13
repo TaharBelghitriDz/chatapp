dee097f93cb4ec1933d437fd18c2e0a11ccef7b5
tahar belghitri <103012653+TaharBelghitriDz@users.noreply.github.com>
2022-07-13 23:00:37 +0100

    Update global.styles.ts

--- frontend/src/styles/global.styles.ts
+++ frontend/src/styles/global.styles.ts
@@ -1,9 +1,18 @@
-import { ThemeConfig } from "@chakra-ui/react";
-
 export const global = {
   body: {
     bg: "#FAFAFA",
     color: "#404040",
     fontFamily: "'Josefin Sans', sans-serif",
+  },
+};
+
+export const customTheme = {
+  fonts: {
+    body: `'Open Sans', sans-serif`,
+    heading: `'Lora', serif`,
+  },
+  color: {
+    red: "#A41623",
+    orange: "#FF7F11",
   },
 };
