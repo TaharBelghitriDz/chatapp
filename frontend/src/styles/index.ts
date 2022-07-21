af1d663d4117602d402738fa822decca1354878c
tahar belghitri <103012653+TaharBelghitriDz@users.noreply.github.com>
2022-07-21 22:31:34 +0100

    Update index.ts

--- frontend/src/styles/index.ts
+++ frontend/src/styles/index.ts
@@ -5,6 +5,16 @@ const theme = extendTheme({
   styles: {
     global,
   },
+
+  colors: {
+    red: "#A41623",
+    orange: "#303030",
+    white: "#FAFAFA",
+    black: {
+      1: "#ffffff",
+    },
+  },
+
   fonts: {
     body: `'Open Sans', sans-serif`,
     heading: `'Lora', serif`,
