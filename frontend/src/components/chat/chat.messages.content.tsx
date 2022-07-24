e9e351ffe150d6d7d3aee67704980648ddc45328
tahar belghitri <103012653+TaharBelghitriDz@users.noreply.github.com>
2022-07-24 17:25:25 +0100

    loading

--- frontend/src/components/chat/chat.messages.content.tsx
+++ frontend/src/components/chat/chat.messages.content.tsx
@@ -1,4 +1,15 @@
-import { Box, HStack, Text, VStack } from "@chakra-ui/react";
+import {
+  Box,
+  HStack,
+  Text,
+  Textarea,
+  useOutsideClick,
+  VStack,
+} from "@chakra-ui/react";
+import { motion, useAnimationControls, useMotionValue } from "framer-motion";
+import { useRef, useState } from "react";
+import chakraHelper from "../../helper/chakraui.helper";
+import { IconSend, IconFilePlus } from "@tabler/icons";
 
 const ChatHeader = () => {
   return (
@@ -27,37 +38,139 @@ const ChatHeader = () => {
 };
 
 const MessageInput = () => {
+  const [isWriting, setWriting] = useState(false);
+  const heightController = useAnimationControls();
+  const ref = useRef(null);
+  useOutsideClick({
+    ref,
+    handler() {
+      heightController.start({ backgroundColor: "rgb(250,250,250,0%)" });
+      setWriting(false);
+    },
+  });
+
+  const setBackground = () => {
+    if (isWriting)
+      heightController.start({ backgroundColor: "rgb(250,250,250,100%)" });
+    else heightController.start({ backgroundColor: "rgb(250,250,250,100%)" });
+
+    setWriting(!isWriting);
+  };
+
   return (
-    <Box
+    <HStack
+      ref={ref}
       pos="absolute"
       bottom="3%"
-      w="90%"
-      left="5%"
-      bg="red"
+      w="full"
+      bg="rgb(250,250,250,50%)"
       h="100px"
       rounded="20px"
-    ></Box>
+      backdropFilter="blur(10px)"
+      border="solid 1px #cccccc"
+      fontSize="20px"
+      justifyContent="space-between"
+      px="20px"
+    >
+      <Textarea
+        as={motion.textarea}
+        resize={"none"}
+        w="80%"
+        h="3Opx"
+        rows={1}
+        focusBorderColor="none"
+        animate={heightController}
+        variant="flushed"
+        border="none"
+        fontSize="20px"
+        placeholder="write you text here"
+        color="#000000"
+        onClick={() => setBackground()}
+        overflow="scroll"
+        bg="rgb(238,238,238,0%)"
+        p="10px"
+        outline="none"
+        rounded="10px"
+        _placeholder={{ color: "#000000" }}
+        onChange={() =>
+          heightController.start({ backgroundColor: "rgb(250,250,250,100%)" })
+        }
+        css={chakraHelper.scrollBar}
+      />
+      <Box
+        as={IconFilePlus}
+        h="30px"
+        w="30px"
+        rounded="full"
+        cursor="pointer"
+      />
+      <Box
+        as={IconSend}
+        h="40px"
+        w="40px"
+        color="#fafafa"
+        cursor="pointer"
+        p="7px"
+        rounded="5px"
+        bg="#4F86C6"
+      />
+    </HStack>
+  );
+};
+
+const ChatMessagesText = (props: { other: boolean }) => {
+  return (
+    <HStack
+      w="full"
+      alignItems="flex-start"
+      flexDirection={props.other ? "row-reverse" : "row"}
+      pr="10px"
+    >
+      <Box h="50px" w="50px" bg="gray" rounded="10px" ml="10px" />
+      <Text
+        maxW="400px"
+        minH="50px"
+        fontSize="20px"
+        p="15px"
+        bg={props.other ? "#C4D4E8" : "#cccccc"}
+        rounded="15px"
+      >
+        hi its tahar and hi its tahar and hi its tahar and hi its tahar and hi
+        hi its tahar and hi its tahar and hi its tahar and hi its tahar and its
+        hi its tahar and hi its tahar and hi its tahar and hi its tahar and hi
+        hi its tahar and hi its tahar and its tahar and hi its tahar and tahar
+        and
+      </Text>
+    </HStack>
   );
 };
 
 const ChatMessages = () => {
   return (
-    <Box
+    <VStack
       h="calc(100% - 120px)"
-      bg="#303030"
       w="full"
-      rounded="20px"
-      position="relative"
+      overflow="scroll"
+      css={chakraHelper.scrollBar}
+      pb="200px"
+      spacing="30px"
     >
-      <MessageInput />
-    </Box>
+      <ChatMessagesText other={false} />
+      <ChatMessagesText other={false} />
+      <ChatMessagesText other={false} />
+      <ChatMessagesText other={false} />
+      <ChatMessagesText other={false} />
+      <ChatMessagesText other={false} />
+      <ChatMessagesText other={true} />
+    </VStack>
   );
 };
 
 export const ChatContent = () => {
   return (
-    <VStack w="50vw" spacing="20px" h="inherit">
+    <VStack w="700px" spacing="20px" h="inherit" position="relative">
       <ChatHeader />
+      <MessageInput />
       <ChatMessages />
     </VStack>
   );
