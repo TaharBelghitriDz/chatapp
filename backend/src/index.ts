1e6e30d93e047a688cf833cfe569a23ff17b88b7
tahar belghitri <103012653+TaharBelghitriDz@users.noreply.github.com>
2022-08-02 13:56:18 +0100

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,18 +3,16 @@ import helmet from "helmet";
 import cors from "cors";
 import db from "../config/mongo.config";
 import { graphqlHTTP } from "express-graphql";
-import { notFoundError, reqErrHandler } from "./middlewares/reqErorHandler";
 import config from "../config";
 import { createServer } from "http";
-import schema from "./schemas.graphql/index.scheams.graphql";
 import { Server } from "socket.io";
 import socketIoConfog from "../config/socketio.confog";
-import socket from "./routes/socketio.routes";
-import { checkToken } from "./middlewares/socketio.middleware";
-import { msg } from "./models/messages.model";
 import { graphqlUploadExpress } from "graphql-upload";
-import { user } from "./models/user.model";
 import { addResolversToSchema } from "@graphql-tools/schema";
+import { checkToken } from "./middlewares/socketio.middleware";
+import socket from "./routes/socketio.routes";
+import { notFoundError, reqErrHandler } from "./middlewares/reqErorHandler";
+import schema from "./schemas/graphql/index.scheams.graphql";
 import resolvers from "./resolvers";
 
 async () => await db;
@@ -43,9 +41,5 @@ const io = new Server(server, socketIoConfog);
 io.path("/");
 io.use(checkToken);
 io.on("connection", socket);
-
-msg.find({}, (_: any, e: any) => {
-  console.log(e);
-});
 
 export default server;
